4cc8a95c4961cc181126d37405e87582119b5daa
Eduardo Silva <eduardo.ejs@hotmail.com>
2018-12-13 16:35:03 -0200

    Consumindo API com VueJs

--- social/src/pages/login/Login.vue
+++ social/src/pages/login/Login.vue
@@ -8,9 +8,9 @@
     <span slot="principal">
 
       <h2>Login</h2>
-      <input type="email" placeholder="E-Mail" value="">
-      <input type="password" placeholder="Senha" value="">
-      <button type="button" class="btn waves-effect waves-light">Entrar</button>
+      <input type="email" placeholder="E-Mail" v-model="usuario.email">
+      <input type="password" placeholder="Senha" v-model="usuario.password">
+      <button class="btn waves-effect waves-light" v-on:click="login()">Entrar</button>
       <router-link to="/cadastro" class="btn orange waves-effect waves-light">Cadastre-se</router-link>
 
     </span>
@@ -20,16 +20,43 @@
 
 <script>
   import LoginTemplate from '@/templates/LoginTemplate'
+  import axios from 'axios';
 
   export default {
     name: 'Login',
     data () {
       return {
-
+        usuario:{email:'', password:''}
       }
     },
     components:{
       LoginTemplate
+    },
+    methods:{
+      login(){
+
+        axios.post('http://127.0.0.1:8000/api/login', {
+          email: this.usuario.email,
+          password: this.usuario.password
+        })
+        .then(response => {
+          console.log(response);
+          if(response.data.token){
+            //login com sucesso
+            console.log('login sucesso');
+          }else if(response.data.status == false){
+            //login não existe
+            console.log('login nao existe');
+          }else{
+            //errors de validação
+            console.log('erro de validacao');
+          }
+        })
+        .catch(e => {
+          //this.errors.push(e)
+          console.log(e);
+        })
+      }
     }
   }
 </script>
